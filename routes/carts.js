b13c639978b13e97c6aab81d6b369a23eac84fdd
Emma Dawson <emma.l.dawson@gmail.com>
2020-09-16 18:22:32 +0200

    Add template for shopping cart

---
+++ routes/carts.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const router = express.Router();
+
+//Add to cart
+router.post("/", async (req, res) => {
+
+});
+
+//Show all items in cart
+router.get("/", async (req, res) => {
+
+});
+
+//Delete item from cart
+router.post("/", async (req, res) => {
+
+});
+
+
+
+module.exports = router;
+
+
